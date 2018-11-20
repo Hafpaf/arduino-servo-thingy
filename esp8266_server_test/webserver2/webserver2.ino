09eb402c70a43cd7bd46b49671a4686c618a81f0
hafpaf <haf@hafnium.me>
2018-11-20 19:57:44 +0100

    add pin D7 and D8

--- esp8266_server_test/webserver2/webserver2.ino
+++ esp8266_server_test/webserver2/webserver2.ino
@@ -5,11 +5,13 @@
 
 ESP8266WebServer Webserver(80);
 
+//Wifi SSID and Passwd
 const char* ssid = "Labitat (free)";
 const char* password = "labitatisawesome";
 
+//important to add variables
 enum stage_names {
-  ledON, ledOFF
+  ledON, ledOFF, drive_forward, drive_backward
 };
 static stage_names stage = ledON;
 
@@ -22,12 +24,13 @@ void setup(void){
   HTMLpage += "<head><title>Webserver Tutorial</title></head><h3>ESP8266 Webserver Demo (Toggle LED)</h3><p>LED <a href=\"ledON\"><button>ON</button></a>&nbsp;<a href=\"ledOFF\"><button>OFF</button></a></p>";
 */
 
+//HTML code
 void HTMLpage() {
   digitalWrite(LED, 1);
   String out;
-  out = "<html><head><title>Labitat test webserver wifi light</title></head>";
-  out += "<font color=\"#021640\"><body bgcolor=\"#ff8d33\">"; 
-  out += "<body style='background-color:#ff8d33'>"; // ffe633 = yellow for bedroom
+  out = "<html><head><title>EL lab wifi enabled webserver</title></head>";
+  out += "<font color=\"#021640\"><body bgcolor=\"#ff8d33\">";
+  out += "<body style='background-color:#ff8d33'>";
   out += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">";
   out += "<FONT SIZE=-1>";
   out += "<h2>Labitat test wifi neopixel light</h2>";
@@ -37,8 +40,8 @@ void HTMLpage() {
   out += "<a href=\"ledOFF\"><button>OFF</button></a>";
   out += "<br/>";
   out += "Drive<br>";
-  out += "<a href=\"ledON\"><button>ON</button></a>";
-  out += "<a href=\"ledOFF\"><button>OFF</button></a>";
+  out += "<a href=\"drive_forward\"><button>Forward</button></a>";
+  out += "<a href=\"drive_backward\"><button>Backward</button></a>";
   out += "<br/>";
 /*  out += "Rainbowcycle<br>";
   out += "<a href=\"rainbowcycle_120\"><button>really slow</button></a>";
@@ -63,11 +66,12 @@ void HTMLpage() {
   out += "<a href=\"lightoff\"><button>-- Light OFF --</button></a><br><br>";*/
   out += "</body>";
   out += "</html>";
-//Webserver.send(200, "text/plain", "Mit navn er ikke Kurt!");
+//Webserver.send(200, "text/plain", "Test text");
   Webserver.send(200, "text/html", out);
   digitalWrite(LED, 0);
 }
 
+//Show if false URL
 void handleNotFound() {
   digitalWrite(LED, 1);
   String message = "File Not Found\n\n";
@@ -108,15 +112,17 @@ void setup(void) {
   if (MDNS.begin("esp8266", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
-
+//Call from HTMLpage and if landed on, forward to void loop(void)
   Webserver.on("/", HTMLpage);
   Webserver.on ( "/ledON", []() {stage=ledON; HTMLpage();} );
   Webserver.on ( "/ledOFF", []() {stage=ledOFF; HTMLpage();} );
+  Webserver.on ( "/drive_forward", []() {stage=drive_forward; HTMLpage();} );
+  Webserver.on ( "/drive_backward", []() {stage=drive_forward; HTMLpage();} );
 
   Webserver.on("/", [](){
     Webserver.send(200, "text/html", out);
-/*
   });
+  /*
   Webserver.on("/ledON", [](){
     Webserver.send(200, "text/html", HTMLpage+"<p>DRIVE</p>");
     digitalWrite(D7, HIGH);
@@ -128,7 +134,7 @@ void setup(void) {
     digitalWrite(D8, HIGH);
     delay(1000);
   });
-*/
+
   });
   Webserver.on("/ledON", [](){
     Webserver.send(200, "text/html", out+"<p>LED is ON</p>");
@@ -141,23 +147,35 @@ void setup(void) {
     digitalWrite(LED, HIGH);
     delay(1000);
   });
+*/
   Webserver.onNotFound(handleNotFound);
 
   Webserver.begin();
   Serial.println("HTTP Webserver started");
 }
 
-void loop(void){
-  Webserver.handleClient();
-    if (stage == ledON) {
-    Webserver.send(200, "text/html", out+"<p>LED is ON</p>");
-    digitalWrite(LED, LOW);
-    delay(1000);
-  } else if (stage == ledOFF) {
-    Webserver.send(200, "text/html", out+"<p>LED is OFF</p>");
-    digitalWrite(LED, HIGH);
-    delay(1000);
-//  } else if (stage == s_yellow) {
+
+//Output, where the magic happens.
+void loop(void) {
+    Webserver.handleClient();
+      if (stage == ledON) {
+      Webserver.send(200, "text/html", out+"<p>LED is ON</p>");
+      digitalWrite(LED, LOW);
+      delay(100);
+    } else if (stage == ledOFF) {
+      Webserver.send(200, "text/html", out+"<p>LED is OFF</p>");
+      digitalWrite(LED, HIGH);
+      delay(100);
+    } else if (stage == drive_forward) {
+      Webserver.send(200, "text/html", out+"<p>Driving forward</p>");
+      digitalWrite(D7, HIGH);
+      delay(100);
+    } else if (stage == drive_backward) {
+      Webserver.send(200, "text/html", out+"<p>Driving backward</p>");
+      digitalWrite(D8, HIGH);
+      delay(100);
+    }
+//  else if (stage == s_yellow) {
 //    colorWipe(strip.Color(255, 255, 0), 20);
-   }
+//  }
 }
