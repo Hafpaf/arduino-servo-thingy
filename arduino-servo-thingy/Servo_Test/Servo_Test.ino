5a96dc2cd914d877022778c788d65febe278bba2
Bombdevelops <37332620+Bombdevelops@users.noreply.github.com>
2018-11-30 15:56:02 +0100

    Updated WORKS!

---
+++ arduino-servo-thingy/Servo_Test/Servo_Test.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int pos = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(D1); //attaches to specefik pin
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  for (pos = 0; pos <= 180; pos += 1){ //goes from 0 to 180 degrees
+  myservo.write(pos);
+  delay(15);
+  }
+}
+
---
+++ arduino-servo-thingy/sketch_Motorstyring/sketch_Motorstyring.ino
@@ -0,0 +1,43 @@
+#include <Keyboard.h>
+#include <HID.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  
+  Keyboard.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  //LeftArrow
+  if Keyboard.press(0xD8){
+    digitalWrite(D1, HIGH);
+    else if Keyboard.release(0xD8){
+      digitalWrite(D1, LOW);
+    }
+  } 
+    
+  
+
+  //RightArrow
+  if Keyboard.press(0xD7){
+    digitalWrite(D2, HIGH);
+    else if digitalWrite(D2, LOW);
+  }
+
+
+  // UpArrow
+  if Keyboard.press(0xDA){
+    digitalWrite(D1 , HIGH) and DigitalWrite(D2,HIGH);
+    else digitalWrite(D1 , LOW) and DigitalWrite(D2,LOW);
+  }
+  
+
+  
+  
+}
