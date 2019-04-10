b718c56e3f0475929ba7e86bf2fa76f31cb29f45
NathiNugget <nathaniel.risum2@gmail.com>
2019-04-10 15:49:36 +0200

    added 3 servo, pins set,

--- ESP32/esp32_servo/esp32_servo.ino
+++ ESP32/esp32_servo/esp32_servo.ino
@@ -6,12 +6,16 @@
 //#include <ESP8266WebServer.h>
 //#include <ESP8266mDNS.h>
 //#include <Servo.h>
+#include <cstdlib>
+
 
 
 //WiFiServer server(80);
 
 Servo myservo;    //laver servo til obj.
-Servo myservo2; 
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
 
 int pos = 0;
 
@@ -26,7 +30,7 @@ const char* password = "testnet1";
 //Set web serverens port nr til 80
 WiFiServer server(80);
 
-//variablen til at gemme HTTP 
+//variablen til at gemme HTTP
 String header;
 
 //få HTTP value
@@ -36,20 +40,24 @@ int pos1 = 0;
 int pos2 = 0;
 
 void setup() {
-  pinMode(13, OUTPUT);
-  pinMode(14, OUTPUT);
+  pinMode(32, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(21, OUTPUT);
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  myservo.attach(13);   //Sætter Servo objekt til servopin
-  myservo2.attach(14);
+  myservo.attach(32);   //Sætter Servo objekt til servopin
+  myservo2.attach(22);
+  myservo3.attach(33);
+  myservo4.attach(21);
 
 
   //Connect to Wi-Fi network med SSID og kodeordet.
   Serial.print("forbinder til ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -66,18 +74,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   WiFiClient client = server.available(); //bruges til signal fra client
 
-  if(client){
+  if (client) {
     Serial.println("Ny Client.");
 
     String currentLine = "";
 
-    while(client.connected()) {   //While loop for clients forbindelse
-      if(client.available()) {    //if bytes for at læse client
+    while (client.connected()) {  //While loop for clients forbindelse
+      if (client.available()) {   //if bytes for at læse client
         char c = client.read();   //læs byte
         Serial.write(c);          //print seriel monitor
-        header += c;              
+        header += c;
         if (c == '\n') {             //hvis byte er ny linje character
-          if(currentLine.length() == 0){  //HTTP
+          if (currentLine.length() == 0) { //HTTP
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
@@ -87,53 +95,78 @@ void loop() {
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-          //CSS Til style ved på og fra knap
+            //CSS Til style ved på og fra knap
 
-          client.println("<style>body{ text-align: center; font-family:\"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
-          client.println(".slider{ width: 300px;}</style>");
-          client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
-          //Web siden
-          client.println("</head><body><h1>ESP32 Servo controller</h1>");
-          client.println("<p>Position: <span id=\"servoPos\"></span></p>");          
-          client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");
-          
-          client.println("<script>var slider = document.getElementById(\"servoSlider\");");
-          client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
-          client.println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
-          client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
-          client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
+            client.println("<style>body{ text-align: center; font-family:\"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
+            client.println(".slider{ width: 300px;}</style>");
+            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
+            //Script indlæses
+            client.println("</head><body><h1>ESP32 Servo controller</h1>");
+            client.println("<p>Position: <span id=\"servoPos1\"></span></p>");
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider1\" onchange=\"servo(this.value)\" value=\"" + valueString + "\"/>");
 
-          client.println("</body></html>");
+            client.println("<p>Position: <span id=\"servoPos2\"></span></p>");
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider2\" onchange=\"servo(this.value)\" value=\"" + valueString + "\"/>");
 
-          //Få value og HTTP/1.1
-          if(header.indexOf("GET /?value=")>=0){
-            pos1 = header.indexOf('=');
-            pos2 = header.indexOf('&');
-            valueString = header.substring(pos1+1, pos2);
-            valueString2 = header.substring(pos1+1, pos2);
-
-            //Rotere servo
-            myservo.write(valueString.toInt());
-            myservo2.write((valueString2.toInt()));
+            client.println("<script>var slider1 = document.getElementById(\"servoSlider1\");");     
+            client.println("var servoP1 = document.getElementById(\"servoPos1\"); servoP1.innerHTML = slider1.value;");
+            client.println("slider1.oninput = function() { slider1.value = this.value; servoP1.innerHTML = this.value; }");
+            client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
+            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}");
+            //Slider 2 
             
-          }
-          //HTTP Response
-          client.println();
-          // Break out of the while loop
-          break;
+            client.println("var slider2 = document.getElementById(\"servoSlider2\");");     
+            client.println("var servoP2 = document.getElementById(\"servoPos2\"); servoP2.innerHTML = slider2.value;");
+            client.println("slider2.oninput = function() { slider2.value = this.value; servoP2.innerHTML = this.value; }");
+            client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
+            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}");
+            
+            
+           
+            
+            
+            
+            client.println("</script>");
+
+
+ 
+            client.println("</body></html>");
+
+            //Få value og HTTP/1.1
+            if (header.indexOf("GET /?value=") >= 0) {
+              pos1 = header.indexOf('=');
+              pos2 = header.indexOf('&');
+              pos3 = header.indexOf('t');
+              pos4 = header.indexOf('q');
+              valueString = header.substring(pos1 + 1, pos2);
+              valueString2 = header.substring(pos3 + 1, pos4);
+
+              //Rotere servo
+              myservo.write(valueString.toInt());
+              myservo2.write((180)-(valueString.toInt()));
+              myservo3.write(valueString.toInt());
+              myservo4.write((180)-(valueString.toInt()));
+              Serial.println(valueString.toInt());
+              
+            }
+            //HTTP Response
+            client.println();
+            // Break out of the while loop
+            break;
           } else {//hvis ny linje, så fjern currentline
             currentLine = "";
           }
-        } else if (c != '\r'){ //hvis du for alt andet end carriage så return character,
-            currentLine += c; //tilføj til currentline
-          }
+        } else if (c != '\r') { //hvis du for alt andet end carriage så return character,
+          currentLine += c; //tilføj til currentline
         }
       }
+    }
     //Clear Header variablen
     header = "";
     //afbryd forbindelse
     client.stop();
     Serial.println("Client afbrød");
     Serial.println("");
+
   }
 }
